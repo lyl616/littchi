715989b1d7afa67d871fb792c25ccc77d1d1bbd5
lyl616 <178916861@qq.com>
2016-09-05 16:30:14 +0800

    Change-Id: I3f6e4161ebdd68211cd8b890cc16928b30bbc3fd

--- litchi-manager-web/src/main/java/com/litchi/controller/PageController.java
+++ litchi-manager-web/src/main/java/com/litchi/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-	@RequestMapping
+	@RequestMapping("/")
 	public String showIndex() {
 		return "index";
 	}
